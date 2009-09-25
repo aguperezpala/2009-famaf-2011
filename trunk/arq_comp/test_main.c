27a00ffc0966e74b467203d6a4c547e0f3cf4f16
marianitovlk <marianitovlk@02179a90-8b6e-11de-a519-b78fc8232508>
2009-09-25 04:56:11 +0000

    Mejore un poco el tad string.
    string_get_back se simplifico a una llamada de string_get_front () ajustada.
    
    Todas las pre / post completas.
    
    Subido el informe, en plain text por ahora, luego hay que pasarlo a pdf (No esta completo).
    
    Se podria simplificar aún mas el código porque slice_* () se asemejan a get_* () solo que 
    no modifican el string_pointer, por ahora tenemos codigo repetido, quizás luego lo 
    retoque (quizas no)
    
    Por último,  alguien lee esto?

--- trunk/arq_comp/test_main.c
+++ trunk/arq_comp/test_main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "strin.h"
-#include "assert.h"
+#include "win_string.h"
+#include "no_std_assert.h"
+
+#define POS 1
+#define LEN 6
 
 int main () {
 
@@ -11,13 +14,14 @@ int main () {
   string *str = NULL;
   
   cstr = "Zapallo";
+  printf ("-- %s --\n",cstr);
   
   str = string_create (cstr);  
   ASSERT (str != NULL);
   
-  result = string_get_back (str, 4, 6);
-  printf ("%s \n",result);
-  free (result);
+  
+  
+  
   
   result = string_slice_right (str, 0, 6);
   printf ("%s \n",result);
