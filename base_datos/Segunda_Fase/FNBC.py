9897c674402489c605a66b78ff967bdd51136206
Hobborg <Hobborg@02179a90-8b6e-11de-a519-b78fc8232508>
2009-10-11 19:42:28 +0000

    Actualizando el diagrama para reflejar los tipos de datos de cada componente que pensamos

--- base_datos/Segunda_Fase/FNBC.py
+++ base_datos/Segunda_Fase/FNBC.py
@@ -8,29 +8,63 @@ def calcular_FNBC (conjRi, Fpri):
 	if (not (type(conjRi) is set)):
 		return "El 1º parametro debe ser un 'set' de 'sets'"
 	elif (not (type(Fpri) is set)):
-		return "El 2º parametro debe ser un 'set' de 'sets'"
+		return "El 2º parametro debe ser un 'set' de 'tuplas'"
 	
-	Ri = conjRi.pop
-	
+"""	Ri = conjRi.pop	
 	if (not (type(Ri) is set)):
-		return "El 1º set debe tener sets dentro suyo"
+		return "El 1º parámetro debe tener sets dentro suyo"
 	else:
 		conjRi.add(Ri)
-	
-	# conjRi es modificable => se pasa por referencia => lo
-	# modificaremos hasta dejarlo en FNBC
+"""	
+	#conjRi es modificable => se pasa por referencia => lo
+	#modificaremos hasta dejarlo en FNBC
 	
 	stop = False
 	while (not stop):
 		stop = True
 		
 		for dep in Fpri:	
-			Ri,depv = es_violación_FNBC (conjRi, dep)
+			Ri = es_violación_FNBC (conjRi, dep)
 			if Ri is not None: # Si hay violación en algún Ri
 				stop = False
-				convertir_FNBC (conjRi, Ri, depv)
+				convertir_FNBC (conjRi, Ri, dep)
+		
+	print "conRi esta ahora en FNBC"
+	
 
 
 
 def es_violación_FNBC (conjRi, dep):
 	
+	attr = dep[0].union(dep[1]) # atributos en la dependencia
+	
+	for Ri in conjRi:
+		if attr.issubset(Ri):
+			
+
+
+
+def convertir_FNBC (conjRi, Ri, dep):
+	
+	conjRi.remove(Ri)
+	
+	Rj = dep[1].union(dep[1]) # atributos en la dependencia
+	
+	for attr in dep[2]:
+		Ri = Ri.remove(attr)
+	
+	
+
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
