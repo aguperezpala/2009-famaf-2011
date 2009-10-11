a0323f752d324e295d47a42eb0d0774f9c5fd469
Hobborg <Hobborg@02179a90-8b6e-11de-a519-b78fc8232508>
2009-10-11 18:57:27 +0000

    Avanzando, o eso espero

--- trunk/base_datos/Segunda_Fase/FNBC.py
+++ trunk/base_datos/Segunda_Fase/FNBC.py
@@ -2,14 +2,33 @@
 
 def calcular_FNBC (conjRi, Fpri):
 
-	"""Cálculo de la forma normal de Boyce Codd de un
+	"""Descomposición en la forma normal de Boyce-Codd de un
 	   conjunto de esquemas relacionales, para un F+ dado"""
 	
 	if (not (type(conjRi) is set)):
-		return "1º parameter must be a set of sets!"
+		return "El 1º parametro debe ser un 'set' de 'sets'"
 	elif (not (type(Fpri) is set)):
-		return "2º parameter must be a set of sets!"
-
+		return "El 2º parametro debe ser un 'set' de 'sets'"
+	
+	Ri = conjRi.pop
+	
+	if (not (type(Ri) is set)):
+		return "El 1º set debe tener sets dentro suyo"
+	else:
+		conjRi.add(Ri)
+	
+	# conjRi es modificable => se pasa por referencia => lo
+	# modificaremos hasta dejarlo en FNBC
+	
+	stop = False
+	while (not stop):
+		stop = True
+		
+		for dep in Fpri:	
+			Ri,depv = es_violación_FNBC (conjRi, dep)
+			if Ri is not None: # Si hay violación en algún Ri
+				stop = False
+				convertir_FNBC (conjRi, Ri, depv)
 
 
 
