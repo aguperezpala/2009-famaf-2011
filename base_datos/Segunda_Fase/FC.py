62758f7818506e45bdfb1e094c6e5f37cde0c6fc
Drasky.Vanderhoff <Drasky.Vanderhoff@02179a90-8b6e-11de-a519-b78fc8232508>
2009-10-12 05:23:33 +0000

    Modifique el diagrama por que con el aguso quedamos que le mando conjuntos de tuplas , asi no tengo que hacer ninguna transformacion. Subo la version final del modulo FC.py de recubrimiento canonico. NO LE TOQUEN NADA QUE ESTA PERFECT! cualquier cosa yo me hago cargo

---
+++ base_datos/Segunda_Fase/FC.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#       FC.py
+#       
+#       Copyright 2009 Drasky Vanderhoff <drasky.vanderhoff@gmail.com>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+#	Utilize las operaciones sobre conj' 
+#	{|,&,-} == {unión,intersección,diferencia} las cuales solo devuelven
+#	un valor pero no modifican a los conjuntos sobre los que operan.
+
+def union_partes_izq(R,alfa,F):
+	
+	""" Fusiona todas las dependencias funcionales que tenga a alfa
+		como parte izquierda de la misma """
+		
+	for b1,b2 in R - set(alfa):
+		if (alfa,b1) in F and (alfa,b2) in F:
+			F.remove((alfa,b1))
+			F.remove((alfa,b2))
+			F.add((alfa,b1|b2))
+				 
+def atrib_raros_der(alfa,beta,R,F):
+	
+	""" Elimina y devuelve una lista de los atributos raros de beta """
+	  
+	raros = []
+	for A in beta:
+		# Parecera largo pero mas variables solo complica su escritura.
+		if A in cierre_atrib(alfa,(F-set((alfa,beta)))|set((alfa,beta-A)),R):
+			F.remove((alfa,beta))
+			F.add((alfa,beta-set(A)))
+			raros += [A]
+	return raros
+	
+def atrib_raros_izq(alfa,beta,R,F):
+	
+	""" Elimina y devuelve una lista de los atributos raros de alfa """
+	
+	raros = []
+	for A in alfa:
+		# Chequeamos beta subconjunto del cierre de atributos de alfa-A
+		if beta.issubset(cierre_atrib(alfa-set(A),F,R)):
+			F.remove((alfa,beta))
+			F.add((alfa-set(A),beta))
+			raros += [A]
+	return raros
+						
+def FC(F,R):
+	res = F.copy() # Copio F para modificarlo a gusto.
+	raros = ["I ALWAYS WANT TO BE A LUMBERJACK"] # Inicialización
+	
+	while len(raros) > 0 :# Mientras obtengamos atributos raros 
+		raros = []
+		for alfa in R: # Realizo todas las uniones para cada alfa
+			union_partes_izq(R,alfa,res)
+			for beta in R: # Agrego los atributos raros de cada beta
+				raros += atrib_raros_izq(alfa,beta,R,res)+atrib_raros_der(alfa,beta,R,res)
+
+	return res
