aea6db6bc4e87181ed45c21b272ea7b12f81041e
Drasky.Vanderhoff <Drasky.Vanderhoff@02179a90-8b6e-11de-a519-b78fc8232508>
2009-10-15 20:59:00 +0000

    BLA\!

--- base_datos/Segunda_Fase/c3fn.py
+++ base_datos/Segunda_Fase/c3fn.py
@@ -4,15 +4,15 @@ import copy
 #Vamos a hacer la parte primero del foreach
 
 #Esta funcion va a chequear si algun Ri contiene a la dep. func. a->b
-#CRi = Conjuntos de Ri (sets of sets)
+#LRi = Conjuntos de Ri (sets of sets)
 #dep = Dependencia funcional a->b (es un tubple (a,b))
 # Returns:
 #	True 	si encontramos
 #	False 	caso contrario
-def isDepInRi(CRi, dep):
+def isDepInRi(LRi, dep):
 	cdep = dep.alfa | dep.beta # hacemos una union de a U b = {a,b}
 	# ahora vamos a buscar si encontramos en alguno de los Ri cdep
-	for c in CRi:
+	for c in LRi:
 		if cdep <= c:
 			# si lo encontramos salimos
 			return True
@@ -22,14 +22,14 @@ def isDepInRi(CRi, dep):
 # Requires:
 #	Fc (F canonico = set of tuples, donde cada tuple es (set,set) == (a,b)
 #	que corresponden al a->b)
-#	CRi = Set of Set (Conjunto de Ri's)
+#	LRi = Set of Set (Conjunto de Ri's)
 # Returns: "nada..."
-# En caso de que no exista => agrega automaticamente un Ri al conjunto CRi
-def firstLoop (Fc, CRi):
+# En caso de que no exista => agrega automaticamente un Ri al conjunto LRi
+def firstLoop (Fc, LRi):
 	for depF in Fc:
-		if not isDepInRi(CRi, depF):
+		if not isDepInRi(LRi, depF):
 			#tenemos que agregar el nuevo Ri = {a,b}
-			CRi.add(depF.alfa | depF.beta)
+			LRi.append(depF.alfa | depF.beta)
 
 
 # Ahora vamos a la 2º parte, vamos a verificar si existe Ri con clave Candidata
@@ -38,17 +38,17 @@ def firstLoop (Fc, CRi):
 
 # Funcion que hace esta comparacion (Asegura la reunion sin perdida).
 # Requires:
-#	CRi = conjunto de Ri's
+#	LRi = conjunto de Ri's
 #	CCC = Conjunto de Claves Candidatas :) 
 # Retuns:
 #	True	si se agrego alguna nueva Ri
 #	False	caso contrario (ya existia Clave Ca. € Ri)
-# Esta funcion modifica el CRi en caso de que tengamos que generar una nueva Ri
+# Esta funcion modifica el LRi en caso de que tengamos que generar una nueva Ri
 # (osea que clave_candidata(R) !€ algun Ri para todo Ri)
 # clavale un nombre a la funcion XD
-def reunionWithoutLoss(CRi, CCC):
+def reunionWithoutLoss(LRi, CCC):
 	for cc in CCC:
-		for ri in CRi:
+		for ri in LRi:
 			if cc <= ri:
 				# No hay que hacer naranja
 				return False
@@ -56,7 +56,7 @@ def reunionWithoutLoss(CRi, CCC):
 	# cualquier Clave Candidata ==> Ri+1 = CCC[0]....
 	# Aca podriamos fijarnos si influye en algo tomar cualquiera, la mas
 	# chica, la mas grande, etc... Para optimizar en algo..?
-	CRi.add(CCC[0])
+	LRi.append(CCC[0])
 	return True
 
 
@@ -66,16 +66,16 @@ def reunionWithoutLoss(CRi, CCC):
 # Funcion que va a calcular 3FN.
 # Requires:
 #	Fc 	= Fcanonico
-#	CRi 	= Conjunto de Ri inicial (R universal....)
+#	LRi 	= Conjunto de Ri inicial (R universal....)
 #	CCC 	= Conjunto Claves Candidatas
 # Returns:
-#	CRi's (Descomposicion 3FN)
+#	LRi's (Descomposicion 3FN)
 # TENER EN CUENTA QUE SE MODIFICAN LOS VALORES, NO SE TRABAJA SOBRE COPIA DE LOS
-# MISMOS. (SI NO SE QUIEREN MODIFICAR => trabajar conFc.copy(), CRi.copy()...
-def calculate3FN (Fc, CRi, CCC):
-	firstLoop(Fc, CRi)
-	reunionWithoutLoss(CRi, CCC)
-	return CRi
+# MISMOS. (SI NO SE QUIEREN MODIFICAR => trabajar conFc.copy(), LRi.copy()...
+def calculate3FN (Fc, LRi, CCC):
+	firstLoop(Fc, LRi)
+	reunionWithoutLoss(LRi, CCC)
+	return LRi
 	
 	
 	
--- base_datos/Segunda_Fase/clcncr.py
+++ base_datos/Segunda_Fase/clcncr.py
@@ -5,15 +5,15 @@
 # y devuelve un NUEVO conjunto de claves candidatas.
 # Requires:
 #	R 	= esquema universal (Set)
-#	CCA	= Conj. de Cierre atributos (Set of tubples => (attr's, cierre))
+#	LCA	= List. de Cierre atributos (Set of tubples => (attr's, cierre))
 # Returns:
-#	CCC	= Conjunto de Claves Candidatas
-def getCCC(R, CCA):
-	CCC = set()
-	for ca in CCA:
+#	LCC	= List. de Claves Candidatas
+def getCCC(R, LCA):
+	LCC = set()
+	for ca in LCA:
 		# verificamos si el cierre (ca[1]) es igual a R
 		if ca[1] <= R <= ca[1]:
 			#si son iguales => la agregamos al conjunto
 			# agregamos una copia por las dudas...
-			CCC.add(ca[0].copy())
-	return CCC
+			LCC.add(ca[0].copy())
+	return LCC
