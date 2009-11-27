1dea21f781f734330ca06c3a2c1b650d4ffa87ce
hobborg <hobborg@02179a90-8b6e-11de-a519-b78fc8232508>
2009-11-27 23:25:34 +0000

    Mas dominios y cambios generales

--- trunk/base_datos/Tercera_Fase/crea_dominios.sql
+++ trunk/base_datos/Tercera_Fase/crea_dominios.sql
@@ -17,15 +17,15 @@ CREATE DOMAIN parentezco AS VARCHAR
 
 
 CREATE DOMAIN estado_civil AS VARCHAR
-	CHECK ( (VALUE LIKE 'Soltero')		OR
-		(VALUE LIKE 'Divorciado')	OR
-		(VALUE LIKE 'Viudo')		OR
-		(VALUE LIKE 'Concubino')	OR
+	CHECK ( (VALUE LIKE 'Soltero')	  OR
+		(VALUE LIKE 'Divorciado') OR
+		(VALUE LIKE 'Viudo')	  OR
+		(VALUE LIKE 'Concubino')  OR
 		(VALUE LIKE 'Casado') );
 
 
 CREATE DOMAIN nivel_instruccion AS VARCHAR
-	CHECK ( (VALUE LIKE 'Ninguna')		OR
+	CHECK ( (VALUE LIKE 'Sin Instruccion')	OR
 		(VALUE LIKE 'Basica')		OR
 		(VALUE LIKE 'Bachiller')	OR
 		(VALUE LIKE 'Tecnico medio')	OR
@@ -33,3 +33,50 @@ CREATE DOMAIN nivel_instruccion AS VARCHAR
 		(VALUE LIKE 'Post grado')	OR
 		(VALUE LIKE 'Tecnico superior')	OR
 		(VALUE LIKE 'Universitario') );
+
+
+CREATE DOMAIN forma_cobro AS VARCHAR
+	CHECK ( (VALUE LIKE 'Diario')	 OR
+		(VALUE LIKE 'Semanal')	 OR
+		(VALUE LIKE 'Quincenal') OR
+		(VALUE LIKE 'Mensual') );
+
+
+CREATE DOMAIN forma_tenencia AS VARCHAR
+	CHECK ( (VALUE LIKE 'Traspasada') OR
+		(VALUE LIKE 'Alquilada')  OR
+		(VALUE LIKE 'Compartida') OR
+		(VALUE LIKE 'Propia')     OR
+		(VALUE LIKE 'Invadida')   OR
+		(VALUE LIKE 'Otr_') );
+
+
+CREATE DOMAIN tipo_vivienda AS VARCHAR
+	CHECK ( (VALUE LIKE 'Quinta')	   OR
+		(VALUE LIKE 'Casa')	   OR
+		(VALUE LIKE 'Rancho')	   OR
+		(VALUE LIKE 'Apartamento') OR
+		(VALUE LIKE 'Pent House') 
+		(VALUE LIKE 'Otr_') );
+
+
+CREATE DOMAIN telefonia AS VARCHAR
+	CHECK ( (VALUE LIKE 'Domiciliaria')	OR
+		(VALUE LIKE 'Celular')		OR
+		(VALUE LIKE 'Prepago') );
+
+
+--CREATE DOMAIN medios AS 
+
+
+CREATE DOMAIN enfermedades AS VARCHAR
+	CHECK ( (VALUE LIKE 'Cancer')		OR
+		(VALUE LIKE 'Corazon')		OR
+		(VALUE LIKE 'Epilepsia')	OR
+		(VALUE LIKE 'Diabetes')		OR
+		(VALUE LIKE 'Hepatitis')	OR
+		(VALUE LIKE 'Tuberculosis')	OR
+		(VALUE LIKE 'Sida')		OR
+		(VALUE LIKE 'Leucemia')		OR
+		(VALUE LIKE 'Hipertension')	OR
+		(VALUE LIKE 'Otra') );
