dd5e5f78cb0d292c177fd1ba3006190fe47e665a
aguperezpala <aguperezpala@02179a90-8b6e-11de-a519-b78fc8232508>
2010-05-01 23:10:43 +0000

    terminado mechanic y washingm

--- modelos/mechanic.c
+++ modelos/mechanic.c
@@ -1,17 +1,39 @@
 #include "mechanic.h"
 
 struct _mechanic {
-	int rrt; /* Tiempo absoluto hasta la proxima maquina que sale 
-		    del taller */
-	GQueue rq; /* Cola de espera de reparacion */
+	int rrt; 	/* Tiempo absoluto hasta la proxima maquina que sale 
+			 * del taller */
+	GQueue *rq; 	/* Cola de espera de reparacion */
+	double TR;	/* tiempo medio de reparacion (1/lambda) */
 };
 
 /* Funcion constructora del mecanico
+* REQUIRES:
+* 	tr	tiempo medio de reparacion (TR)
 * RETURNS:
 * 	NULL 			on error
 * 	mechanic != NULL	if success
 */
-mechanic_t mechanic_create(void);
+mechanic_t mechanic_create(double tr)
+{
+	mechanic_t result = NULL;
+	
+	result = (mechanic_t) malloc(sizeof(*result));
+	if (!result)
+		return NULL;
+	
+	/* creamos ahora la cola */
+	result->rq = g_queue_new();
+	if(!result->rq) {
+		perror("Error al crear la cola\n");
+		free(result);
+		return NULL;
+	}
+	result->rrt = -1;
+	result->TR = tr;
+	
+	return result;
+}
 
 /* Funcion que devuelve una washing machine en caso de que haya una reparada
 * o NULL en caso contrario.
@@ -22,21 +44,90 @@ mechanic_t mechanic_create(void);
 * 	wm	si hay maquina reparada
 * 	NULL	si no hay 
 */
-wm_t mechanic_get_rm(mechanic_t m, int month);
+wm_t mechanic_get_rm(mechanic_t m, int month)
+{
+	wm_t wm = NULL;
+	
+	
+	if (!m) {
+		printf("Error, estamos recibiendo un mecanico NULL\n")
+		return NULL;
+	}
+	
+	/* vamos a verificar si ya esta arreglada la maquina */
+	if (m->rrt == month) {
+		/* SI esta arreglada ==> la extraemos y la devolvemos */
+		wm = (wm_t) g_queue_pop_head(m->rq);
+		
+		if (!wm) 
+			/* que paso aca? como puede ser... */
+			printf("Algo esta andando mal en el mecanico\n");
+		else {
+			/* debemos ver ahora si tenemos que ponernos a reparar
+			 * la otra ahora, o si no tenemos ninguna no hacemos
+			 * nada */
+			if (g_queue_is_empty(m->rq))
+				/* no hay nada que reparar */
+				m->rrt = -1;
+			else {
+				/* si hay maquinas => determinamos en cuanto
+				 * tiempo va a ser reparada, recordemos que 
+				 * la media de la exponencial es 1/lambda. */
+				m->rrt = gen_exp((double) 1.0/m->TR);
+			}
+	}
+	
+	/* si no tenemos ninguna maquina devolvemos NULL, si no, devolvemos la
+	 * primera en la cola */
+	
+	return wm;
+}
 
 /* Funcion que agrega una maquina a reparar
 * REQUIRES:
 * 	m 	!= NULL
 * 	wm	!= NULL
 */
-void mechanic_repair_machine(mechanic_t m, wm_t wm);
+void mechanic_repair_machine(mechanic_t m, wm_t wm)
+{
+	if (!m || ! wm) {
+		printf("Intentando agregar una maquina en un mecanico NULL "
+			" o intentando meter una maquina NULL\n");
+		return;
+	}
+	
+	/* veamos si tenemos arreglando una maquina.. osea si la cola es
+	 * distinta de vacio */
+	if (g_queue_is_empty(m->rq))
+		/* la arreglamos ya ... y veamos cuanto va a tardar.. */
+		m->rrt = gen_exp((double) 1.0/m->TR);
+	else
+		/* hay maquinas arreglandose, simplemente la encolamos */
+		g_queue_push_tail(m->rq, wm);
+}
 
+/* Funcion que devuelve la cantidad de maquinas que tiene el mecanico 
+* actualmente (tanto en la cola como la que esta reparando
+* REQUIRES:
+* 	m != NULL
+*/
+int mechanic_get_n_rmachines(mechanic_t m)
+{
+	if (!m)
+		return -1;
+	return g_queue_get_length(m->rq);
+}
 
 /* Funcion destructora 
 * REQUIRES:
 * 	m 	!= NULL
 */
-mechanic_t mechanic_destroy(mechanic_t m);
-
+mechanic_t mechanic_destroy(mechanic_t m)
+{
+	if (m)
+		free(m);
+	
+	return NULL;
+}
 
 
--- modelos/mechanic.h
+++ modelos/mechanic.h
@@ -6,16 +6,19 @@
 #include <glib.h>
 
 #include "washingm.h"
+#include "rg.h"		/* donde tenemos 
 
 typedef struct _mechanic * mechanic_t;
 
 
 /* Funcion constructora del mecanico
+ * REQUIRES:
+ * 	tr	tiempo medio de reparacion (TR)
  * RETURNS:
  * 	NULL 			on error
  * 	mechanic != NULL	if success
  */
-mechanic_t mechanic_create(void);
+mechanic_t mechanic_create(double tr);
 
 /* Funcion que devuelve una washing machine en caso de que haya una reparada
  * o NULL en caso contrario.
@@ -35,6 +38,12 @@ wm_t mechanic_get_rm(mechanic_t m, int month);
  */
 void mechanic_repair_machine(mechanic_t m, wm_t wm);
 
+/* Funcion que devuelve la cantidad de maquinas que tiene el mecanico 
+ * actualmente (tanto en la cola como la que esta reparando
+ * REQUIRES:
+ * 	m != NULL
+ */
+int mechanic_get_n_rmachines(mechanic_t m);
 
 /* Funcion destructora 
  * REQUIRES:
