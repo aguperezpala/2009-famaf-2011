7be9e57c1fd0681f711b26661b9fb7da02931846
aguperezpala <aguperezpala@02179a90-8b6e-11de-a519-b78fc8232508>
2010-05-01 23:38:13 +0000

    agregando rg

---
+++ modelos/rg.h
@@ -0,0 +1,24 @@
+#ifndef RANDOM_GENERATORS_H
+#define RANDOM_GENERATORS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+
+
+/* Funcion que genera una variable aleatoria de Poisson con parametro
+ * lambda */
+int rg_gen_poisson(double lambda);
+
+/* generadora de una v.a. exponencial con parametro lambda */
+double rg_gen_exp(double lambda);
+
+/* funcion que genera una v.a. uniforme discreta en el intervalo [a,b] */
+int rg_gen_uni_dis(int a, int b);
+
+/* funcion que genera una v.a. uniforme continua en (a,b) */
+double rg_gen_uni_cont(double a, double b);
+
+
+#endif
