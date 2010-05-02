3e7c20d6f26bc055a237fb9459e3e59b29d88b3d
Hobborg <Hobborg@02179a90-8b6e-11de-a519-b78fc8232508>
2010-05-02 03:25:12 +0000

    Avanzando con el RAD laundry

--- modelos/laundry.h
+++ modelos/laundry.h
@@ -5,4 +5,14 @@
 typedef struct _laundry *laundry_t;
 
 
+/* Devuelve el tiempo en el que ocurrió el último fallo del sistema
+ * REQUIRES:
+ *	l != NULL
+ * RETURNS
+ * 	ftime > 0 , si el sistema falló tras "ftime" meses de operación
+ * 	ftime < 0 , si el sistema aún no falló
+ */
+int laundry_get_failure_time (laundry_t l);
+
+
 #endif
