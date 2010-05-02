803f52c5dbb53bf2bfa88c0fe77db13b1cc6a2eb
hobborg <hobborg@02179a90-8b6e-11de-a519-b78fc8232508>
2010-05-02 16:32:26 +0000

    Listos el creador y el destructor del TAD laundry

--- trunk/modelos/laundry.h
+++ trunk/modelos/laundry.h
@@ -1,8 +1,38 @@
+/* TAD laundry, en español "lavandería"
+ *
+ * Contiene las máquinas lavadoras operativas, las de servicio
+ * y el conjunto de mecánicos en el taller de reparación.
+ * Lleva además la cuenta de los meses transcurridos desde su creación
+ */
+
+
 #ifndef LAUNDRY_H
 #define LAUNDRY_H
 
 
+
 typedef struct _laundry *laundry_t;
+
+
+
+/* Creador de TAD. Deberá luego ser liberado con el destructor aquí provisto
+ * Especificación de los parámetros:
+ *	Nop:	# de máquinas operativas
+ *	Nserv:	# de máquinas de servicio
+ *	Nmech:	# de mecánicos en el taller de reparación
+ *	Tf:	tiempo medio de fallo de una lavadora operativa
+ *	Tr	tiempo medio de reparación de un mecánico
+ */
+laundry_t laundry_create (uint Nop, uint Nserv, uint Nmech, uint Tf, uint Tr);
+
+
+/* Destructor del TAD
+ * PRE:
+ *	l != NULL
+ * POS:
+ *	todos los recursos de memoria de 'l' fueron liberados
+ */
+void laundry_destroy (laundry_t l);
 
 
 /* Lleva a cabo la operación normal de la lavandería durante un mes, es decir:
@@ -13,8 +43,7 @@ typedef struct _laundry *laundry_t;
  * PRE:
  *	l != NULL
  */
-void wash_clothes (laundry_t *l);
-
+void laundry_wash_clothes (laundry_t l);
 
 
 /* Incrementa en uno el mes actual de la lavandería
@@ -23,10 +52,7 @@ void wash_clothes (laundry_t *l);
  * POS:
  *	el tiempo de operación de la lavandería fue incrementado en una unidad
  */
-int laundry_increase_month (laundry);
-
-
-
+int laundry_increase_month (laundry_t laundry);
 
 
 /* Indica si la lavandería dejó de ser operativa, ie: si tiene menos máquinas
@@ -34,7 +60,7 @@ int laundry_increase_month (laundry);
  * PRE:
  *	l != NULL
  */
-bool laundry_failure (laundry_t *l);
+bool laundry_failure (laundry_t l);
 
 
 /* Devuelve el tiempo en el que ocurrió el último fallo del sistema
