b18e1c0da5738ad6f0cc7072534b2c1186841b61
Hobborg <Hobborg@02179a90-8b6e-11de-a519-b78fc8232508>
2010-06-08 23:06:59 +0000

    Modularizando el calculo de valores estadisticos para los valores muestrales (media muestral y varianza muestral)

--- modelos/Guias/6/p_2.c
+++ modelos/Guias/6/p_2.c
@@ -2,63 +2,15 @@
 #include <sys/time.h>
 #include <math.h>
 #include "rdg.h"
-
+#include "ssv.h"
 
 #define PAD	40	/* Definido para arquitectura de 64 bits */
-#define SIZE 2
 
 double	TOL = 0.01;	/* Nivel de tolerancia deseado: S/sqrt(n) < TOL */
 
-/** Media muestral */
-double	media[SIZE] = {0.0, 0.0};
-unsigned int	ma = 0,  /* media[ma] = media anterior  = X(n-1) */
-ms = 0;  /* media[ms] = media siguiente = X(n)   */
-
-/** Varianza muestral */
-double	var[SIZE] = {0.0, 0.0};		/* Varianza muestral */
-unsigned int	va = 0,  /* var[va] = varianza anterior  = S^2(n-1) */
-vs = 0;  /* var[vs] = varianza siguiente = S^2(n)   */
-
-
-/* Media muestral para el n-esimo paso. Toma una nueva muestra Xn y
-* devuelve el "proximo" valor de la media muestral = X(n)
-*/
-static double media_m (double Xn, double n)
-{
-	/* Recordar:	media[ma] = media anterior  = X(n-1) *
-	*		media[ms] = media siguiente = X(n)   *
-	*						     */
-	
-	ma = ms;
-	ms = (ms+1) % SIZE;
-	media[ms] = media[ma] + (Xn-media[ma]) / n;
-	
-	return media[ms];
-}
-
-
-/* Varianza muestral para el n-esimo paso. Toma una nueva muestra Xn y
-* devuelve el "proximo" valor de la varianza muestral = S^2(n)
-*
-* NOTE: ASUME que (media[ma] , media[ms]) tienen los valores (X(n-1) , X(n))
-* 	 respectivamente (ver arriba la funcion mm, "Media muestral")
-*/
-static double var_m (double Xn, double n)
-{
-	/* Recordar:	var[va] = varianza anterior  = S^2(n-1) *
-	 *		var[vs] = varianza siguiente = S^2(n)	*
-	 *							*/
-	
-	va = vs;
-	vs = (vs+1) % SIZE;
-	var[vs] = ((n - 1.0) / n) * var[va] + \
-	(n + 1.0) * pow (media[ms] - media[ma], 2.0);
-	
-	return var[vs];
-}
-
 
 static double f (double x) { return exp (x*x); }
+
 
 int main (void)
 {
@@ -82,17 +34,9 @@ int main (void)
 	if (idum > 0)
 		idum = -idum;
 	
-	/* El 1er paso lo realizamos afuera del ciclo general	*
-	 * por issues con la generacion de la varianza muestral */
-	
-	U = ran2(&idum);
-	Xn = f(U);
-	est += Xn;
-	
-	n = 1.0;
-	X = media_m (Xn, n);
 	
 	/* Ciclo principal de simulacion */
+	n = 0.0;
 	while (n < 100.0 || S/sqrt(n) > TOL) {
 		
 		/* Generamos un nuevo dato y lo acumulamos */
