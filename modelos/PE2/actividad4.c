f7e20dd9d44357baa1985b8292dc2bbc6462b351
aguperezpala <aguperezpala@02179a90-8b6e-11de-a519-b78fc8232508>
2010-06-21 06:48:55 +0000

    subiendo generador de pi

--- modelos/PE2/actividad4.c
+++ modelos/PE2/actividad4.c
@@ -4,6 +4,27 @@
 #include <math.h>
 #include <stdio.h>
 
+
+/* Funcion que genera el valor de la integral en un intervalo entre a y b
+ * y la funcion fun.
+ */
+static double act4_monte_carlo(double a, double b, double (*fun)(double))
+{
+	double U = 0;
+	double acum = 0;
+	int i = 0;
+	//MONTECARLO_BOUND
+	
+	for(i = 0; i < MONTECARLO_BOUND; i++) {
+		U = mzran13()/(double)ULONG_MAX;
+		U = (U * (b-a)) + a;
+		
+		acum += fun(U);
+	}
+	
+	return (acum/(double)MONTECARLO_BOUND);
+}
+
 /* Funcion que genera intervalos.
 * REQUIRES:
 * 	begin 		comienzo del intervalo
@@ -14,21 +35,47 @@
 * 	< 0		on error
 * 	0		if success
 */
-int act4_gen_intervals(int begin, int end, int n, double *I)
+int act4_gen_intervals(double begin, double end, int n, double *I)
 {
 	double delta = 0;
 	int i = 0;
 	
 	assert(I != NULL);
-	delta = ((double)(end-begin))/(double)n;
+	delta = (end-begin)/(double)n;
 	
 	if(delta <= 0)
 		return -1;
 	
 	I[0] = begin;
 	for(i = 1; i < n; i++)
-		I[i] = (double) begin + ((double)i) * delta;
+		I[i] = begin + ((double)i) * delta;
 	
 	return 0;
 }
+
+/* Funcion que genera las pi para un arreglo de intervalos determinados
+* y una funcion especifica.
+* REQUIRES:
+* 	p		arreglo donde se van almacenar las pi
+* 	I		arreglo de intervalos
+* 	n		cantidad de intervalos
+* 	fun		funcion de densidad
+*/
+void act4_gen_pi(double *p, double *I, int n, double (*fun)(double))
+{
+	double	delta = 0;
+	int 	i = 0;
+	
+	assert(I != NULL);
+	assert(p != NULL);
+	assert(fun != NULL);
+	
+	delta = I[1] - I[0];
+	for (i = 0; i < n; i++) {
+		p[i] = act4_monte_carlo(I[i], I[i] + delta, fun) * delta;
+	}
+	
+	
+}
+
 
--- modelos/PE2/actividad4.h
+++ modelos/PE2/actividad4.h
@@ -1,10 +1,15 @@
 #ifndef ACTIVIDAD4_H
 #define ACTIVIDAD4_H
 
+#define _GNU_SOURCE
+#include <limits.h>
+
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include "rdg.h"
 
+#define MONTECARLO_BOUND	1000
 
 
 /* Funcion que genera intervalos.
@@ -17,8 +22,17 @@
  * 	< 0		on error
  * 	0		if success
 */
-int act4_gen_intervals(int begin, int end, int n, double *I);
+int act4_gen_intervals(double begin, double end, int n, double *I);
 
+/* Funcion que genera las pi para un arreglo de intervalos determinados
+ * y una funcion especifica.
+ * REQUIRES:
+ * 	p		arreglo donde se van almacenar las pi
+ * 	I		arreglo de intervalos
+ * 	n		cantidad de intervalos
+ * 	fun		funcion de densidad
+ */
+void act4_gen_pi(double *p, double *I, int n, double (*fun)(double));
 
 
 
