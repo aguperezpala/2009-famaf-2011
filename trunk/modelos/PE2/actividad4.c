8a6c93ad02075882020fe3748e9dc6f2627fea05
aguperezpala <aguperezpala@02179a90-8b6e-11de-a519-b78fc8232508>
2010-06-21 05:38:53 +0000

    agregando el generador de intervalos

--- trunk/modelos/PE2/actividad4.c
+++ trunk/modelos/PE2/actividad4.c
@@ -1,78 +1,34 @@
+#include "actividad4.h"
+
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
 
-#include "actividad2.h"
-#include "distrib.h"
-
-#include "actividad4.h"
-
-/* Funcion que devuelve el p-valor utilizando la aproximacion ji-cuadrada.
+/* Funcion que genera intervalos.
 * REQUIRES:
-* 	k	= num de segmentos
-* 	arr	= arreglo de valores de la muestra
-* 	size	= tamaño de la muestra
-* 	fun	= H0 (puntero a funcion "double func(double)")
-* 	gl	= grados de libertad
+* 	begin 		comienzo del intervalo
+*	end		fin del intervalo
+*	n		cantidad de intervalos
+*	I		arreglo donde se va a llenar
 * RETURNS:
-* 	p-valor
+* 	< 0		on error
+* 	0		if success
 */
-double get_p_value_ji(int k, double *arr, int size, double (*fun)(double), int gl)
+int act4_gen_intervals(int begin, int end, int n, double *I)
 {
-	int *N = NULL;
-	double 	min = 0,
-		max = 0,
-		delta = 0,
-		T = 0,
-		result = 0;
-		
-	int i = 0, j = 0;
+	double delta = 0;
+	int i = 0;
 	
-	assert(arr != NULL);
-	assert(fun != NULL);
-	assert(size > 0 && k >= 0);
+	assert(I != NULL);
+	delta = ((double)(end-begin))/(double)n;
 	
-	N = (int *) calloc (k, sizeof (int));
-	assert (N != NULL);
+	if(delta <= 0)
+		return -1;
 	
-	/* calculamos el tamaño de los intervalos */
-	min = act2_get_min(arr, size);
-	max = act2_get_max(arr, size);
-	delta = (max - min)/(double)k;
+	I[0] = begin;
+	for(i = 1; i < n; i++)
+		I[i] = (double) begin + ((double)i) * delta;
 	
-	/* llenamos los Ni ineficientemente */
-	for(i = 0; i < k; i++)
-		for(j = 0; j < size; j++)
-			if ((arr[j] < ((i+1)*delta)+min) &&
-			     arr[j] >= ((i*delta)+min))
-				N[i]++;
-	
-	/* vamos a calcular T */
-	for(i = 0; i < k; i++)
-		T += pow((N[i] - ((double) size * fun(arr[i]))), 2) * 
-			(1.0/(double)size * fun(arr[i]));
-	
-	result = chi_cuadrada(gl, T);
-	
-	free (N);
-	N = NULL;
-	
-	return result;
+	return 0;
 }
 
-
-/* Funcion que devuelve el p-valor utilizando la aproximacion kolmogorov-Smirnov.
-* REQUIRES:
-* 	k	= num de segmentos
-* 	arr	= arreglo de valores de la muestra
-* 	size	= tamaño de la muestra
-* 	Fun	= H0 (puntero a funcion "double func(double)")NOTE: FH0(acumulada)
-* 	gl	= grados de libertad
-* RETURNS:
-* 	p-valor
-*/
-double get_p_value_ks(int k, double *arr, int size, double (*Fun)(double), int gl)
-{
-	return 0.0;
-	/*! NOTE FIXME: */
-}
