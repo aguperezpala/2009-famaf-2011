7203883fffd74019dfd9d1c89a31387a18152de7
aguperezpala <aguperezpala@02179a90-8b6e-11de-a519-b78fc8232508>
2010-06-21 05:38:53 +0000

    agregando el generador de intervalos

--- modelos/PE2/actividad4.h
+++ modelos/PE2/actividad4.h
@@ -1,32 +1,23 @@
 #ifndef ACTIVIDAD4_H
 #define ACTIVIDAD4_H
 
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
 
-/* Funcion que devuelve el p-valor utilizando la aproximacion ji-cuadrada.
+
+
+/* Funcion que genera intervalos.
  * REQUIRES:
- * 	k	= num de segmentos
- * 	arr	= arreglo de valores de la muestra
- * 	size	= tamaño de la muestra
- * 	fun	= H0 (puntero a funcion "double func(double)")
- * 	gl	= grados de libertad
+ * 	begin 		comienzo del intervalo
+ *	end		fin del intervalo
+ *	n		cantidad de intervalos
+ *	I		arreglo donde se va a llenar
  * RETURNS:
- * 	p-valor
- */
-double get_p_value_ji(int k, double *arr, int size, double (*fun)(double), int gl);
-
-/* Funcion que devuelve el p-valor utilizando la aproximacion kolmogorov-Smirnov.
-* REQUIRES:
-* 	k	= num de segmentos
-* 	arr	= arreglo de valores de la muestra
-* 	size	= tamaño de la muestra
-* 	Fun	= H0 (puntero a funcion "double func(double)")NOTE: FH0(acumulada)
-* 	gl	= grados de libertad
-* RETURNS:
-* 	p-valor
+ * 	< 0		on error
+ * 	0		if success
 */
-double get_p_value_ks(int k, double *arr, int size, double (*Fun)(double), int gl);
-
-
+int act4_gen_intervals(int begin, int end, int n, double *I);
 
 
 
