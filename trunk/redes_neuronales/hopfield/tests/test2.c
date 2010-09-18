97c147d13a9a4a9625288089f3b36ebd07faca2f
Hobborg <Hobborg@02179a90-8b6e-11de-a519-b78fc8232508>
2010-09-18 05:07:46 +0000

    Trabajando sobre test2.c Este programita medira las velocidades de actualizacion de la red comparando 2 formas de almacenamiento:   a) Una neurona por posicion del arreglo S   b) Una neurona por cada bit de cada posicion de S
       A su vez la situacon a) exige que cada memoria XI[mu] guarde una componente
    (ie: XI[mu][i] = +1,-1) en cada posicion del arreglo. Mientras que b) exige que
    cada memoria XI[mu] guarde una componente en cada bit de cada posicion.

--- trunk/redes_neuronales/hopfield/tests/test2.c
+++ trunk/redes_neuronales/hopfield/tests/test2.c
@@ -2,39 +2,108 @@
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
+#include <inttypes.h>
+#include "../mzran13.h"
 
+#define  _byte_size  (1<<3)
+#define  MOD  (_byte_size*sizeof(long))
+
+/* N y P deben ser multiplos de MOD */
 #define  N  (1<<13)
 #define  P  (N/2)
+#define  NU  1
+
+
+/* Normalización a (-1,+1) */
+#define  norm(x)  ((x) > 0 ? 1 : -1)
+
+/* Acceso a una matriz de dimensiones [P][N] */
+#define  AT(i,j)  ((i)*P + (j)%N)
+
+
+
+/* Inicializa aleatoriamente las p memorias en XI
+ * PRE: XI != NULL
+ */
+static void init_mems (int *XI)
+{
+	long i = 0;
+	uint64_t aux = 0;
+	
+	assert (XI != NULL);
+	
+	/* NOTE El no determinismo del scheduler se escurre por aqui en la
+	* paralelizacion del ciclo, haciendo que las memorias NO se generen
+	* de manera identica entre llamadas sucesivas al programa.
+	*	Es decir: invocaciones consecutivas del programa resultaran
+	* en resultados de XI[] diferentes cada vez.
+	*/
+	#pragma omp parallel for shared(XI)
+	for (i=0 ; i<N*p ; i++) {
+		aux = mzran13() % 2;
+		XI[AT(i/N,i%N)] = norm(aux);
+	}
+	
+	return;
+}
+
+
+/* Inicializa el estado 'S' en una posicion aleatoriamente cercana a XI[NU]
+ * PRE: S != NULL
+ */
+static void init_state (int *S)
+{
+	int i = 0;
+	uint64_t aux = 0;
+	
+	assert (S != NULL);
+	
+	#pragma omp parallel for shared(XI)
+	for (i=0 ; i<N ; i++) {
+		aux = mzran13() % 2;
+		S[i] = aux > 0 ? XI[AT(NU,i)] : XI[AT(NU,1)];
+	}
+	
+	return;
+}
+
+
+
 
 int main (void)
 {
-	int *mem_i = NULL, *XI = NULL, *S = NULL;
-	double *mem_d = NULL;
-	int mu = 0, i = 0;
+	long *Sbit = NULL;	/* Estado con 1 neurona x bit */
+	long *Spos = NULL;	/* Estado con 1 neurona x posicion */
+	long *XIbit = NULL;	/* Memorias con 1 lugar x bit */
+	long *XIpos = NULL;	/* Memorias con 1 lugar x posicion */
+	int *m = NULL;		/* Superposicion memoria/estado */
 	clock_t start=0, end=0;
 	
 	
 	/* Generamos los arreglos */
-	mem_i = (int *) calloc (P, sizeof(int));
-	assert (mem_i != NULL);
+	Sbit = (long *) calloc (N/MOD, sizeof(long));
+	assert (Sbit != NULL);
 	
-	mem_d = (double *) calloc (P, sizeof(double));
-	assert (mem_d != NULL);
+	Spos = (long *) calloc (N, sizeof(long));
+	assert (Spos != NULL);
 	
-	XI = (int *) calloc (N, sizeof(int));
-	assert (XI != NULL);
+	XIbit = (long *) calloc (P*(N/MOD), sizeof(long));
+	assert (XIbit != NULL);
 	
-	S = (int *) calloc (N, sizeof(int));
-	assert (S != NULL);
+	XIpos = (long *) calloc (P*N, sizeof(long));
+	assert (XIpos != NULL);
 	
 	
 	/* Inicializamos el estado y las memorias */
-	for (i=0 ; i<N ; i++)
-		/** TODO: inicializar el estado 'S'
-		 **	  inicializar las memorias 'XI'
-		 **	  inicializar las superposiciones 'mem'
-		 **/
-		
+	/** TODO
+	 **	1) Generar rutinas de inicializacion para los arreglos *bit
+	 **	2) Reutilizar init_mems e init_state para los arreglos *pos
+	 **	3) Inicializar los arreglos *bit y *pos
+	 **	4) Realizar MAX_ITER ciclos de actualizacion en cada caso
+	 **	5) Imprimir el (tiempo medido / MAX_ITER) en ambos casos
+	 **/
+	init_mems (XI);
+	init_state (S);	
 		
 		
 	/* Prueba con enteros */
@@ -54,7 +123,7 @@ int main (void)
 		mem_d[mu] += XI[mu] * S[i];
 	}
 	end = clock ();
-	printf ("\nUsando m[mu] de tipo INT se tardo: %.4f seg\n",
+	printf ("\nUsando m[mu] de tipo DOUBLE se tardo: %.4f seg\n",
 		(double)end-start/(double)CLOCKS_PER_SEC);
 		
 	free (mem_i);	mem_i = NULL;
