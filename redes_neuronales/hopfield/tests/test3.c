ef37410f9df3c89a8734a953774d5abed856a1d9
Hobborg <Hobborg@02179a90-8b6e-11de-a519-b78fc8232508>
2010-09-18 21:21:14 +0000

    Generando test3.c donde jugamos con la operacion bitxbit

---
+++ redes_neuronales/hopfield/tests/test3.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <limits.h>
+#include <inttypes.h> /* To have access to uuint64_t */
+#include "../mzran13"
+
+
+#define  _byte_size  (1<<3)
+#define  MOD  (_byte_size*sizeof(unsigned long))
+
+
+uint64_t mask[MOD];
+
+inline static void set_mask ()
+{
+	mask[0] = 1;
+	for (int i=1; i<MOD ; i++)
+		mask[i] |= mask[i-1] | ((long unsigned)1)<<i;
+}
+
+/* Print n as a binary number */
+void printbits (uint64_t n) {
+	uint64_t l = 0;
+	short k = 1;
+	
+	while (l > 0) {
+		printf("%c", n&l ? '1' : '0');
+		l >>= 1;
+		!(k++ % _byte_size) ? printf(" ") : 0;
+	}
+	printf ("\n");
+}
+
+
+int main (void)
+{
+	long int i = 0;
+	uint64_t aux = 0, tmp = 0;
+	unsigned long S = 0;
+	unsigned long XI = 0x0123456789ABCDEF;
+	/* XI == (MSB) 0000 0001 0010 0011 0100 0101 0110 0111 ...
+	 *	   ... 1000 1001 1010 1011 1100 1101 1110 1111 (LSB) */
+	
+/**	Example of how printbits works */
+/*	set_mask();
+	for (i=0 ; i<MOD ; i++) {
+		printf ("mask[%ld]:\n", i);
+		printbits(mask[i]);
+	}
+*/
+	
+	for (i=0 ; i<MOD ; i++) {
+		aux = mzran13();
+		if (aux % 2)
+			/* tmp = ξ(i) */
+			tmp = ((unsigned long)1) << (MOD-1-i);
+		else
+			/* tmp = ξ(1) */
+			tmp = ((unsigned long)1) << (MOD-1);
+		tmp &= XI;
+		S |= tmp;
+	}
+	printf ("XI: ");
+	printbits (XI);
+	printf ("S:  ");
+	printbits (S);
+	
+	return 0;
+}
