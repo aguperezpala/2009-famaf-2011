ee9e73cc1c1c2b21f9777fa93c1bbec083295310
Hobborg <Hobborg@02179a90-8b6e-11de-a519-b78fc8232508>
2010-09-30 17:44:44 +0000

    Añadiendo tolerancia para tamaños de red N que no sean múltiplos de 64 \n + reformateo de single_result_plotting.gp para facilitar su uso \n + añadido paralelismo en el overlap-updating, y quitado del XI_initialization

--- trunk/redes_neuronales/hopfield/practico1.sh
+++ trunk/redes_neuronales/hopfield/practico1.sh
@@ -2,34 +2,34 @@ LOG=redes_neuronales_practico1.log
 
 # ____________________________________________________________________________ #
 ############################## EJERCICIO 1 #####################################
-
-echo -n "________________________________________________________________________________"
-echo -e "\nEjercicio 1: Hopfield determinístico\n\nCompilando las fuentes ..."
-make clean &> $LOG
-make det >> $LOG 2>&1
-echo -e "Fuentes compiladas\n"
-
-N=448
-P=224
-h=2
-echo "Ejecutando caso N = $N, Pmax = $P, hop = $h"
-./deterministic_Hopfield $N $P $h &> hop_det_n$N\_p$P\_h$h.dat
-
-N=832
-P=416
-h=4
-echo "Ejecutando caso N = $N, Pmax = $P, hop = $h"
-./deterministic_Hopfield $N $P $h &> hop_det_n$N\_p$P\_h$h.dat
-
-N=1664
-P=832
-h=8
-echo "Ejecutando caso N = $N, Pmax = $P, hop = $h"
-./deterministic_Hopfield $N $P $h &> hop_det_n$N\_p$P\_h$h.dat
-
-echo "Fin del caso determinístico."
-echo -e "Salidas en los archivos hop_det_n?_p?_h?.dat\n"
-
+# 
+# echo -n "________________________________________________________________________________"
+# echo -e "\nEjercicio 1: Hopfield determinístico\n\nCompilando las fuentes ..."
+# make clean &> $LOG
+# make det >> $LOG 2>&1
+# echo -e "Fuentes compiladas\n"
+# 
+# N=448
+# P=224
+# h=2
+# echo "Ejecutando caso N = $N, Pmax = $P, hop = $h"
+# ./deterministic_Hopfield $N $P $h &> hop_det_n$N\_p$P\_h$h.dat
+# 
+# N=832
+# P=416
+# h=4
+# echo "Ejecutando caso N = $N, Pmax = $P, hop = $h"
+# ./deterministic_Hopfield $N $P $h &> hop_det_n$N\_p$P\_h$h.dat
+# 
+# N=1664
+# P=832
+# h=8
+# echo "Ejecutando caso N = $N, Pmax = $P, hop = $h"
+# ./deterministic_Hopfield $N $P $h &> hop_det_n$N\_p$P\_h$h.dat
+# 
+# echo "Fin del caso determinístico."
+# echo -e "Salidas en los archivos hop_det_n?_p?_h?.dat\n"
+# 
 
 # ____________________________________________________________________________ #
 ############################## EJERCICIO 2 #####################################
@@ -45,49 +45,49 @@ echo "" | awk -v v1=$h -v v2=$T '{ printf ("Se usarán niveles de ruido \
 T ∈ { %.1f , %.1f , ... , %.1f }\n\n", v1, v1*2, v2) }'
 
 # ~~~~~~~~~~~~~~~~~~~~~~~ N = 448 ~~~~~~~~~~~~~~~~~~~~~~~~
-N=448
+N=416
 
-P=20
+P=5
 echo "Ejecutando caso N = $N, P = $P"
 ./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
 
-P=40
+P=10
 echo "Ejecutando caso N = $N, P = $P"
 ./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
 
-P=60
+P=15
 echo "Ejecutando caso N = $N, P = $P"
 ./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
 
 # ~~~~~~~~~~~~~~~~~~~~~~~ N = 832 ~~~~~~~~~~~~~~~~~~~~~~~~
-N=832
-
-P=20
-echo "Ejecutando caso N = $N, P = $P"
-./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
-
-P=40
-echo "Ejecutando caso N = $N, P = $P"
-./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
-
-P=60
-echo "Ejecutando caso N = $N, P = $P"
-./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
+# N=832
+# 
+# P=10
+# echo "Ejecutando caso N = $N, P = $P"
+# ./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
+# 
+# P=20
+# echo "Ejecutando caso N = $N, P = $P"
+# ./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
+# 
+# P=30
+# echo "Ejecutando caso N = $N, P = $P"
+# ./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
 
 # ~~~~~~~~~~~~~~~~~~~~~~~ N = 1664 ~~~~~~~~~~~~~~~~~~~~~~~
-N=1664
-
-P=20
-echo "Ejecutando caso N = $N, P = $P"
-./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
-
-P=40
-echo "Ejecutando caso N = $N, P = $P"
-./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
-
-P=60
-echo "Ejecutando caso N = $N, P = $P"
-./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
+# N=1664
+# 
+# P=20
+# echo "Ejecutando caso N = $N, P = $P"
+# ./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
+# 
+# P=40
+# echo "Ejecutando caso N = $N, P = $P"
+# ./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
+# 
+# P=60
+# echo "Ejecutando caso N = $N, P = $P"
+# ./stochastic_Hopfield $N $P $T $h &> hop_stoc_n$N\_p$P.dat
 
 
 echo "Fin del caso estocástico."
--- trunk/redes_neuronales/hopfield/single_result_plotting.gp
+++ trunk/redes_neuronales/hopfield/single_result_plotting.gp
@@ -1,46 +1,74 @@
+#
 # Print final overlap results of a HOPFIELD NETWORK of a specified size N
 #
 # It takes its input from the specified "FILE", which must be composed of
-# 3 columns: 1) p/N values
-#	     2) mean overlap values
-#	     3) standard deviations
+# 3 columns: 1) x axis values: p/N or T levels
+#	     2) y axis values: mean overlap
+#	     3) standard deviations of the former
 #
-# ~~~ CONFIG. SECTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+DET=0
+STOC=1
+#
+#
+# ~~~ BEGIN CONFIG. SECTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-FILE="hop_stoc_n1664_p20.dat"
-TITLE="Stochastic_hopfield"
-N="1664"
-P="20"
+
+FILE =  "sample_stoc_n416.dat"
+TITLE = "Stochastic_hopfield_2"
+
+N = "416"
+P = "5"
+
+COND = STOC
+# Options for COND: { DET , STOC }
+
+
 
 # ~~~ END CONFIG. SECTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+################################################################################
+#
+# ~~~ BEGIN PLOT SECTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 reset
 
-# set title TITLE offset first -0.16,1.12
-# set label "N = ".N offset first 0.45,1.115 center
-set title TITLE offset first -0.4,1.12
-set label "N = ".N offset first 0.85,1.115 center
-set label "P = ".P offset first 1.05,1.115 center
+if (COND==DET)								\
+	set out TITLE."_".N."_plot.png" ;				\
+	set title TITLE offset first -0.16,1.12 ;			\
+	set label "N = ".N offset first 0.45,1.115 center ;		\
+	set xlabel "α   ( p / N )" ;					\
+	set xr [0.0:0.5] ;						\
+	set yr [0.0:1.05] ;						\
+	set xtics 0.05 ;						\
+	set mxtics 5 ;							\
+									\
+else if (COND==STOC)							\
+	set out TITLE."_".N."_".P."_plot.png" ;				\
+	set title TITLE offset first -0.4,1.12 ;			\
+	set label "N = ".N offset first 0.85,1.115 center ;		\
+	set label "P = ".P offset first 1.05,1.115 center ;		\
+	set xlabel "T (noise level)" ;					\
+	set xr [0.1:1.2] ;						\
+	set yr [-0.05:1.05] ;						\
+	set xtics 0.1 ;							\
+	set mxtics 2 ;							\
+									\
+else									\
+	print "\nChoose a valid option for COND\n" ;			\
+	exit() ;
 
-# set xr [0.0:0.5]
-# set yr [0.0:1.05]
-set xr [0.1:1.2]
-set yr [-0.05:1.05]
-set xtics 0.1
-set mxtics 5
 set ytics 0.1
 set ytics nomirror
 set style line 4 lc rgb "grey"
 set grid ls 4
 set border 1+2+4
 
-set xlabel "T (noise level)" 
 set ylabel "m (overlap)"
 set key box
 set bar 0.3
-
 set term png size 800, 600 nocrop
-set out TITLE."_".N."_".P."_plot.png"
-plot FILE with lines lt -1 lw 3 t "Values" , \
-	FILE u 1:2:3 w errorb lt 2 lw 2 t "Deviations"
+plot FILE with lines lt 3 lw 3 t "Values" , \
+	FILE u 1:2:3 w errorb lt 1 lw 2 t "Deviation"
 
+#
+# ~~~ END PLOT SECTION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
