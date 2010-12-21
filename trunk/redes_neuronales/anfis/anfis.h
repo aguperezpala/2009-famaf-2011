b6ddb5ae5e3cf056aacc4b1ad0f96b2aecad3034
Hobborg <Hobborg@02179a90-8b6e-11de-a519-b78fc8232508>
2010-12-21 00:40:36 +0000

    Seguimos avanzando en el TAD anfis. Añadiendo los headers de 4 funciones para acceso y manipulación de los parámetros internos de la red. Todavía hay que darles cuerpo a esas funciones en el source file, sin mencionar que faltan por hacer todas las rutinas de dinámica de red.

--- trunk/redes_neuronales/anfis/anfis.h
+++ trunk/redes_neuronales/anfis/anfis.h
@@ -15,22 +15,34 @@
 typedef struct _anfis_s *anfis_t;
 
 
+/* Functions return values */
+#define  ANFIS_OK	( 0)
+#define  ANFIS_ERR	(-1)
+
+
+
 /* Quantity of possible membership functions (ie: length of MF_kind enum) */
 #define  MAX_FUNC	4
 /* Maximum # of parameters for any membership function */
 #define  MAX_PARAM	4
 
-typedef enum {
-	triang,
-	trap,
-	gauss,
-	bell
-} MF_kind;
 
-typedef struct {
-	MF_kind   k ;		   /* Membership function's kind */
-	double    p [MAX_PARAM];   /* Membership function's parameters */
-} MF;
+/******   Membership function TAD   *******************************************/
+/**/                                                                        /**/
+/**/   typedef enum {                                                       /**/
+/**/                  triang,                                               /**/
+/**/                  trap,                                                 /**/
+/**/                  gauss,                                                /**/
+/**/                  bell                                                  /**/
+/**/   } MF_kind;                                                           /**/
+/**/                                                                        /**/
+/**/   typedef struct {                                                     /**/
+/**/                    MF_kind  k ;             /* MF's kind */            /**/
+/**/                    double   p [MAX_PARAM];  /* MF's parameters */      /**/
+/**/   } MF;                                                                /**/
+/**/                                                                        /**/
+/******************************************************************************/
+
 
 
 
@@ -63,15 +75,86 @@ anfis_destroy (anfis_t net);
 
 
 
-/** ### ### ### ~~~~~~~~~ NETWORK INITIALIZERS/OBSERVERS ~~~~~~~~~~~~~~~~~~~~ */
+/**#####################    ACCESSORS / OBSERVERS    #########################*/
 
 
 void
 anfis_print (anfis_t net);
 
-/* Prints in STDOUT the internal state of the network
+/* Prints into STDOUT the internal state of the network
  * PRE: net != NULL
  */
+
+
+
+int
+anfis_get_MF (anfis_t net, unsigned int i, unsigned int j, MF *a);
+
+/* Copies into 'a' the network's i-th row membership function,
+ * which evaluates the j-th input component (aka: MF[i][j])
+ *
+ * PRE: net != NULL
+ *	a   != NULL
+ *	i < network # of branches
+ *	j < network input dimension
+ *
+ * POS: result == ANFIS_OK   &&   MF[i][j] data has been copied inside 'a'
+ *	or
+ *	result == ANFIS_OK
+ */
+
+
+
+int
+anfis_set_MF (anfis_t net, unsigned int i, unsigned int j, MF a);
+
+/* Sets to 'a' the network membership function in the i-th row,
+ * which evaluates the j-th input component (aka: MF[i][j])
+ *
+ * PRE:	net != NULL
+ *	i < network # of branches
+ *	j < network input dimension
+ *
+ * POS: result == ANFIS_OK   &&   MF[i][j] has been replaced with 'a'
+ *	or
+ *	result == ANFIS_ERR
+ */
+
+
+
+double *
+anfis_get_P (anfis_t net, unsigned int i);
+
+/* Returns a copy of the network's i-th consequent parameters
+ * Caller owns the memory generated for the returned vector,
+ * which must be freed using glibc standard free() routine
+ *
+ * PRE:	net   != NULL
+ *	i < network # of branches
+ *	
+ * POS: result != NULL   &&   result contains the coefficients of p[i]
+ *	or
+ *	result == NULL
+ */
+
+
+
+int
+anfis_set_P (anfis_t net, unsigned int i, const double *new_p);
+
+/* Sets to 'new_p' the network i-th row consequent parameters (aka: p[i])
+ *
+ * PRE:	net   != NULL
+ *	new_p != NULL
+ *	length_of (new_p) == network input dimension + 1
+ *	i < network # of branches
+ *	
+ * POS: result == ANFIS_OK   &&   coefficients in p[i] have been replaced
+ *				  with those in 'new_p'
+ *	or
+ *	result == ANFIS_ERR
+ */
+
 
 
 #endif
