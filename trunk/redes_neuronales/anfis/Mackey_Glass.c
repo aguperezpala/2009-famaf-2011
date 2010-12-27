9e0a03ac616a0d4259a325bc8e6b748d6138142a
Hobborg <Hobborg@02179a90-8b6e-11de-a519-b78fc8232508>
2010-12-27 05:31:24 +0000

    Empezando con la construcción del trabajo que se presentará

--- trunk/redes_neuronales/anfis/Mackey_Glass.c
+++ trunk/redes_neuronales/anfis/Mackey_Glass.c
@@ -3,23 +3,25 @@
 #include <math.h>
 
 
-/* Estimation interval bounds */
-#define  _a	 118
-#define  _b	1118
+
+double  _a   = 0.0,	/* Estimation lower bound */
+	_b   = 0.0,	/* Estimation upper bound */
+	_h   = 0.1,	/* Time step size */
+	_tau = 17.0,	/* Time delay for the differential equation */
+	_ya  = 1.2;	/* Series first point value */
+
+double *y = NULL;	/* Results array */
+
+
+
+
 /* # of places to overlook when printing */
 #define  margin   50
-/* Time step size */
-#define  _h	 0.1
-/* Time delay for the differential equation */
-#define  _tau	  17
-/* Initial values */
-#define  _y0	 0.0	/* Before the first point */
-#define  _ya	 1.2	/* First point */
+/* Initial values before the first point */
+#define  _y0	 0.0
 
-/* Results array */
-double *y = NULL;
 /* Little trick to access results array correctly */
-#define  time(t)	lround (t * (1.0/_h))
+#define  time(t)	lround ((t) * (1.0/_h))
 
 
 
@@ -27,7 +29,7 @@ double *y = NULL;
 /* Runge-Kutta method for DE series estimation
  *
  * PARAMETERS:	h -------> time step size
- *		t -------> previous time value 
+ *		t -------> previous time value
  *		y_t -----> function's value in time 't'
  *		f(t,y) --> differential equation
  *
@@ -59,15 +61,91 @@ static double f (double t, double y_t)
 
 
 
-int main (void)
+
+
+
+static void
+parse_input (int argc, char **argv,
+	     double *a, double *b, double *h, double *tau, double *ya)
+{
+	char *err = NULL;
+	
+	if (argc != 6) {
+		fprintf (stderr, "Must be called with following arguments:\n"
+				 "\t1) Lower estimation bound\n"
+				 "\t2) Upper estimation bound\n"
+				 "\t3) Time step size\n"
+				 "\t4) Delay size (smaller than arg #1)\n"
+				 "\t5) Series first value, ie: y(a)\n");
+		exit (EXIT_FAILURE);
+	}
+	
+	/* Retrieving estimation bounds */
+	*a = strtod (argv[1], &err);
+	if (err[0] != '\0') {
+		fprintf (stderr, "Error at '%s' while parsing input\n"
+				 "Must give lower bound of estimation "
+				 "as first argument\n", err);
+		exit (EXIT_FAILURE);
+	}
+	
+	*b = strtod (argv[2], &err);
+	if (err[0] != '\0') {
+		fprintf (stderr, "Error at '%s' while parsing input\n"
+				 "Must give upper bound of estimation "
+				 "as second argument\n", err);
+		exit (EXIT_FAILURE);
+	}
+	
+	*h = strtod (argv[3], &err);
+	if (err[0] != '\0') {
+		fprintf (stderr, "Error at '%s' while parsing input\nMust give "
+				 "time step size as third argument\n", err);
+		exit (EXIT_FAILURE);
+	}
+	
+	*tau = strtod (argv[4], &err);
+	if (err[0] != '\0') {
+		fprintf (stderr, "Error at '%s' while parsing input\nMust give "
+				 "delay size as fourth argument\n", err);
+		exit (EXIT_FAILURE);
+	} else if (*tau > *a) {
+		fprintf (stderr, "Delay size must be smaller than arg #1\n");
+		exit (EXIT_FAILURE);
+	}
+	
+	*ya = strtod (argv[5], &err);
+	if (err[0] != '\0') {
+		fprintf (stderr, "Error at '%s' while parsing input\nMust give "
+				 "series first value as fifth argument\n", err);
+		exit (EXIT_FAILURE);
+	}
+	
+	if (*a > *b) {	/* swap values */
+		*a += *b;
+		*b  = *a - *b;
+		*a -= *b;
+	}
+	
+	return;
+}
+
+
+
+int main (int argc, char **argv)
 {
 	int i = 0;
-	double t = 0.0, new_val = 0.0;
-	size_t a = 0, b = 0;
+	size_t  a = 0,    b = 0;
+	double  t = 0.0, new_val = 0.0;
+	
+	
+	parse_input (argc, argv, &_a, &_b, &_h, &_tau, &_ya);
+	
 	
 	/* Creating work environment  */
-	a = lround (ceil (_a * (1.0/_h)));
-	b = lround (ceil (_b * (1.0/_h)));
+	a = time(_a);
+	b = time(_b + margin);
+	
 	y = (double *) calloc (b, sizeof (double));
 	if (y == NULL) {
 		perror ("Mackey-Glass: insufficient memory to execute");
@@ -83,7 +161,7 @@ int main (void)
 	
 	/* Estimating series */
 	t = _a;
-	while (t < _b) {
+	while (t < _b + margin) {
 		new_val = RungeKutta (t, y[time(t)], _h, f);
 		t += _h;
 		y[time(t)] = new_val;
@@ -91,9 +169,10 @@ int main (void)
 	
 	/* Printing results */
 	t = _a + margin;
-	i = a + lround (margin * (1.0/_h));
-	for (; i < b ; i++) {
-		printf ("%.1f\t%f\n", t, y[time(t)]);
+	a += time(margin);
+	for (i = a ; i < b ; i++) {
+		if (!(i % ((int)(1.0/_h))))
+			printf ("%.1f\t%f\n", t, y[time(t)]);
 		t += _h;
 	}
 	
