044e3b7a25b38f36ebfd73363d03a066fe9090b5
hobborg <hobborg@02179a90-8b6e-11de-a519-b78fc8232508>
2011-01-20 06:14:43 +0000

    Arreglado el problema de histéresis al final de los aprendizajes. Se debía a un bug en main.c a la hora de generar la muestra que se usaría para el exercise_network. Estábamos saliéndonos del arreglo 'y' y eso hacía que los valores resulten en cualquier cosa.
    Modificada la impresión de resultados. Ahora se imprime por un lado la serie
    original de Mackey-Glass superpuesto con el aprendizaje de la red. Y por el
    otro lado el error de este aprendizaje.
    
    A pesar de las modificaciones hechas hasta ahora no hay cambio en la calidad del
    aprendizaje. Sigue siendo mucho peor de lo que dice el paper de Jang. No creo
    que el error esté en el algoritmo de anfis, ya revisé todo muchas veces.
    ¿Tendrá que ver con el modo en que alimentamos los ejemplos de entrenamiento?

--- redes_neuronales/anfis/trabajo_final.sh
+++ redes_neuronales/anfis/trabajo_final.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 LOG=anfis.log
+MAIN=./anfis_mg
 
-# Generación de muestra
+# Serie de aprendizaje
 GEN_MG=./Mackey_Glass
 PLOT_MG=Mackey_Glass.gp
 MG_DATA=Mackey_Glass.dat
+MG_ANFIS=anfis.dat
 MG_PLOT=Mackey_Glass.png
 
-# Aprendizaje ANFIS
-MAIN=./anfis_mg
+# Error de aprendizaje del ANFIS
 PLOT_LERR=learn_error.gp
-LERR_DATA=anfis.dat
-LERR_PLOT=anfis.png
+LERR_DATA=anfis_err.dat
+LERR_PLOT=anfis_err.png
 
 echo "________________________________________________________________________________"
 echo -e "\n\tRedes ANFIS para predicción temporal de series caóticas\n"
@@ -20,33 +21,34 @@ echo -e "    Análisis del aprendizaje para la equación diferencial de Mackey-G
 
 a=118
 b=1118
-margin=50
 h=0.1
 tau=17
 yinit=1.2
 
-echo -e "\nGenerando valores muestrales de la serie"
+echo -e "\nGenerando valores muestrales de la serie Mackey-Glass"
 make clean &> $LOG
 make mg_sample >> $LOG 2>&1
 $GEN_MG $a $b $h $tau $yinit > $MG_DATA
-
-echo -e "Graficando los valores generados"
-export XLOW=`expr $a + $margin`
-export XHIGH=`expr $b + $margin`
-export SAMPLE=$MG_DATA
-export PLOT=$MG_PLOT
-gnuplot $PLOT_MG
-eog $MG_PLOT &
-echo -e "Datos en $MG_DATA\nGráfico en $MG_PLOT"
 
 echo -e "\nCompilando programa principal"
 make anfis_mg >> $LOG 2>&1
 # Lo siguiente guarda en $nlines el # de líneas del archivo MG_DATA
 nlines=`wc -l $MG_DATA | tr -c -d [0-9]`
 echo "Ejecutando programa principal"
-$MAIN $MG_DATA $nlines $LERR_DATA
+$MAIN $MG_DATA $nlines $MG_ANFIS $LERR_DATA
 
-echo "Graficando errores de aprendizaje obtenidos"
+echo -e "Graficando los valores generados vs. la serie original"
+export XLOW=`expr $nlines / 2`
+export XHIGH=$nlines
+export ORIGINAL=$MG_DATA
+export LEARNING=$MG_ANFIS
+export PLOT=$MG_PLOT
+gnuplot $PLOT_MG
+eog $MG_PLOT &
+echo -e "\nDatos originales de la serie de Mackey-Glass en $MG_DATA"
+echo -e "Datos del aprendizaje de la red en $MG_ANFIS\nGráfico en $MG_PLOT"
+
+echo -e "\nGraficando errores de aprendizaje obtenidos"
 export SAMPLE=$LERR_DATA
 export PLOT=$LERR_PLOT
 gnuplot $PLOT_LERR
