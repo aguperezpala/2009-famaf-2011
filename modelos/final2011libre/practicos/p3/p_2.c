c5a7fe7179bf5773bac8854ae591565df4d1e7ec
aguperezpala <aguperezpala@02179a90-8b6e-11de-a519-b78fc8232508>
2011-01-25 00:21:01 +0000

    ,,,,

--- modelos/final2011libre/practicos/p3/p_2.c
+++ modelos/final2011libre/practicos/p3/p_2.c
@@ -12,33 +12,43 @@
 #include "gen_continuas.h"
 #include "monte_carlo.h"
 
-#define PAD 40	/* Definido para arquitectura de 64 bits */
 
+static double func_a(float x) {return pow (1.0-x*x, 1.5 );}
 
+static double func_b(float x) {return x*pow(1+x*x,-2);}
+
+static double func_c(float x) {return exp(-x*x);}
 
 static double func_d(float x1, float x2)
 {
 	return (exp(pow(x1+x2, 2)));
 }
 
-static double f1(float x2)
-{	
-	
-	return pow (1.0-x2*x2, 1.5 );
-	//return pow((double)1 - pow(x,2), ((double)3)/((double)2));
-}
 
-
-int main(void)
+int main2(void)
 {
-	double montecarlo1, montecarlo2;
-	/*
-	montecarlo1 = monte_carlo_normal(99999, f1);
-	montecarlo2 = monte_carlo_ab(99999, f1, 0, 1);
-	*/
+	int iter = 100;
+	double result = 0;
 	
-	montecarlo1 = monte_carlo_multiple(99999999, func_d);
 	
-	printf("montecarlo1: %G\nmontecarlo1: %G\n", montecarlo1, montecarlo2);
+	while (iter <= 1000000) {
+		printf("\n\nIter: %d\n", iter);
+		
+		result = monte_carlo_normal(iter, func_a);
+		printf("a) %G\n", result);
+		
+		result = monte_carlo_0toInf(iter, func_b);
+		printf("b) %G\n", result);
+		
+		result = 2.0 * monte_carlo_0toInf(iter, func_c);
+		printf("c) %G\n", result);
+		
+		result = monte_carlo_multiple(iter, func_d);
+		printf("d) %G\n", result);
+		
+		iter *= 10;
+	}
+	
+	
 	return 0;
 }
