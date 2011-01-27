895cb9302f6a0994846c3f5180b4a58573f30024
aguperezpala <aguperezpala@02179a90-8b6e-11de-a519-b78fc8232508>
2011-01-27 04:05:20 +0000

    ...

--- trunk/modelos/final2011libre/common/gen_discretas.h
+++ trunk/modelos/final2011libre/common/gen_discretas.h
@@ -13,6 +13,8 @@
 #include "rdg.h"
 #include "gen_continuas.h"
 
+typedef double(*FUNC_TI_PTR)(int);
+
 /*!  METODO TRANSFORMADA INVERSA
  * Queremos generar X ~ con func de masa de probabilidad P(X = xj) = pj. Sum pj = 1
  * Para esto generamos U(0,1) y X = { x0 si U < p0, x1 si p0 <= U < p1, en general
@@ -25,6 +27,14 @@
  * NOTE: es mas eficiente si se ordenan los pi en forma decreciente.
  */
 int disc_transf_inversa(double *pi);
+/* Otra opcion es darle la funcion de distribucion para obtener los pi directamente
+ * Aca perdemos eficiencia pero es mas rapido de programar 
+ * NOTE: deberiamos cambiar el puntero de funcion si la funcion toma mas
+ * argumentos... 
+ * El 2` argumento es para indicar desde donde debe comenzar la F[j] argumento 
+ */
+int disc_trasnf_inversa_func(FUNC_TI_PTR funcDistr);
+int disc_trasnf_inversa_func2(FUNC_TI_PTR funcDistr, int j);
 
 
 /* Generacion de una v.a uniforme discreta entre [a,b). 
@@ -52,5 +62,36 @@ int disc_gen_poisson(double lambda);
  */
 int disc_gen_binomial(int n, double p);
 
+
+/*!*****************************************************************************/
+
+
+
+
+/*! Metodo de acceptacion y rechazo:
+ * Se quiere calcular X, con {pj}, j = 1,2,....k Se tiene un metodo eficiente para
+ * generar Y v,a con prob {qj} j=1,2...k Debe cumplir que pj != 0 ==> qj != 0
+ * para todo j. Ademas: c > 1 : pj/qj <= c para todo j. La Y debe adoptar valores
+ * dentro del rango de X.
+ * Toma como argumentos la funcion que genera la func de masa de X : P[X = i],
+ * la funcion que genera los qj = P[Y=j], que en general es una uniforme por lo que
+ * uno de los parametros esta de mas (solo usamos el tamanio y no el i ya que 
+ * para todo i, qj = 1/n). Pero mejos mas generico.
+ */
+int disc_aceptacion_rechazo(double (*funcDistr_Pj)(int), double (*funcDistrRechazo_Qj)(int, int), 
+			    double cota, int k);
+
+
+
+
+/*!*****************************************************************************/
+
+/*
+* Metodo de composicion: sea X = alfa * X1 + (1-alfa) * X2, con P1 func de dens
+* de prob de X1, P2 de X2 (deben cumplir que la sumatoria de las P1j = 1. idem P2j).
+* Usamos la transformada inversa para calcular las P1j, P2j
+* El j es para determinar desde donde comienzan los pj (j = 1...., o j = 0..., etc)
+*/ 
+int disc_metodo_composicion(double (*P1j)(int), double (*P2j)(int), double alfa, int j);
 
 #endif
--- trunk/modelos/final2011libre/practicos/p4/p_5.c
+++ trunk/modelos/final2011libre/practicos/p4/p_5.c
@@ -12,42 +12,81 @@
 #include "gen_continuas.h"
 #include "monte_carlo.h"
 
+#define LAMBDA		2.71828182845904523536028
+#define K_FUNCION	15
+#define NUM_ITER	100
 
-static double func_a(float x) {return pow (1.0-x*x, 1.5 );}
 
-static double func_b(float x) {return x*pow(1+x*x,-2);}
+static double constante = 0;
 
-static double func_c(float x) {return exp(-x*x);}
-
-static double func_d(float x1, float x2)
+static double fact (unsigned long j)
 {
-	return (exp(pow(x1+x2, 2)));
+	unsigned long i=j, res=1;
+	for (i=j ; i>0 ; i--)
+		res *= i;
+	return (double) res;
 }
 
+static double sumatoria_auxiliar(double lda, int k)
+{
+	unsigned int i=0;
+	double res=0.0, fact[2]={1.0,1.0};
+	res += pow (lda, 0.0) / fact[0];
+	
+	for (i=1 ; i<=k ; i++) {
+		fact[i%2] = fact[(i+1)%2] * i;
+		res += pow (lda, (double) i) / fact[i%2];
+	}
+	return res;
+}
+
+static double func_distrib(int i)
+{
+	double result = 1.0 / constante;
+	int k = 1;
+	
+	for(k = 1; k <= i; k++){
+		result *= LAMBDA / (double) k;
+	}
+	
+	return result;
+}
+
+/* funcion usada para los qj, usamos uniforme */
+double distrib_uniforme(int i, int n){
+	/* No usamos i pero lo dejamos por conveniencia, y tenemos que el tamanio
+	 * va de 0 a n, asique debemos devolver 1/(n+1) */
+	return (1./(double)(n+1));
+}
 
 int main5(void)
 {
-	int iter = 100;
-	double result = 0;
+	int iter = 0;
+	double transfInversa = 0, ti = 0;
+	double acceptRechazo = 0, ar = 0;
+	double mu = floor(LAMBDA);
 	
+	constante = sumatoria_auxiliar(LAMBDA, K_FUNCION);
 	
-	while (iter <= 1000000) {
-		printf("\n\nIter: %d\n", iter);
+	double cota = (pow(LAMBDA, mu)/fact(mu))*((K_FUNCION+1)/constante);
+	
+	printf("constante: %f\n", 1./constante);
+	
+	for(iter = 0; iter < NUM_ITER; iter++){
+		ti = disc_trasnf_inversa_func(func_distrib);
+		transfInversa += ti;
+		ar = disc_aceptacion_rechazo(func_distrib, distrib_uniforme, 
+							cota, K_FUNCION);		
+		acceptRechazo += ar;
 		
-		result = monte_carlo_normal(iter, func_a);
-		printf("a) %G\n", result);
-		
-		result = monte_carlo_0toInf(iter, func_b);
-		printf("b) %G\n", result);
-		
-		result = 2.0 * monte_carlo_0toInf(iter, func_c);
-		printf("c) %G\n", result);
-		
-		result = monte_carlo_multiple(iter, func_d);
-		printf("d) %G\n", result);
-		
-		iter *= 10;
+		printf("transfInversa: %f\tacceptRechazo: %f\n", ti, ar);
 	}
+	
+	transfInversa /= (double) NUM_ITER;
+	acceptRechazo /= (double) NUM_ITER;
+	
+	printf("E[transfInversa]: %f\tE[acceptRechazo]: %f\n", transfInversa,
+	       acceptRechazo);
 	
 	
 	return 0;
